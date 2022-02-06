ebd4e36cae91374e605f394207277dbaf7805e6d
NeilBardhan <indraneil.bardhan@gmail.com>
2022-02-05 16:26:19 -0800

    Initial commit

---
+++ src/data.py
@@ -0,0 +1,17 @@
+import os
+import kaggle
+from zipfile import ZipFile
+
+def get_kaggle_competition_data(competition, path):
+    """
+    """
+    kaggle.api.authenticate()
+    f = kaggle.api.competition_list_files(competition)
+    if f:
+        kaggle.api.competition_download_files(competition, path=path, force=True, quiet=False)
+        zf = ZipFile('{p}{c}.zip'.format(p=path, c=competition))
+        zf.extractall(path)
+        zf.close()
+
+get_kaggle_competition_data('widsdatathon2022', os.getcwd() + '/data/')
+
